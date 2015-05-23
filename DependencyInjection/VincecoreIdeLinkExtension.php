8a799a2d619509cf4a88bcd30729779fb2b982f0
Vincent Verhasselt <vincent.verhasselt@gmail.com>
2015-05-23 21:23:53 +0200

    Remove unused code (fix DemoExtension not found bug)

--- DependencyInjection/VincecoreIdeLinkExtension.php
+++ DependencyInjection/VincecoreIdeLinkExtension.php
@@ -3,16 +3,12 @@
 namespace Vincecore\IdeLinkBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Config\FileLocator;
 
 class VincecoreIdeLinkExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
     }
 
     public function getAlias()
